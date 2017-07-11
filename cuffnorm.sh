23826e11b7e8423786415fb2bc892acb8ab3023f
HideoFu <h.fushimi.x689@gmail.com>
2017-07-11 19:15:40 +0900

    Update cuffnorm.sh

--- cuffnorm.sh
+++ cuffnorm.sh
@@ -4,8 +4,6 @@
 # if a sample has multiple bam files, ',' is required between bam filenames.
 # (Can't handle with this script)
 
-cd ..
-
 for INDEX in $*
 do
     list="$list./cuffquant_results/${INDEX}/abundances.cxb "
